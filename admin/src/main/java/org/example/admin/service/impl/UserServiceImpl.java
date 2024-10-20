e94f792d603a823112e8b07d260593bf1d226aa8
black8848 <fuzixian666@gmail.com>
2024-10-20 18:19:15 +0800

    优化：使用布隆过滤器来判断用户名是否存在于数据库中，将查询在 Redis 缓存中进行，避免了直接访问数据库的问题

--- admin/src/main/java/org/example/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/org/example/admin/service/impl/UserServiceImpl.java
@@ -3,18 +3,23 @@ package org.example.admin.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.example.admin.common.convention.exception.ClientException;
 import org.example.admin.common.enums.UserErrorCodeEnum;
 import org.example.admin.dao.entity.UserDO;
 import org.example.admin.dao.mapper.UserMapper;
 import org.example.admin.dto.resp.UserRespDTO;
 import org.example.admin.service.UserService;
+import org.redisson.api.RBloomFilter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 //用户接口实现层
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
 
     @Override
     public UserRespDTO getUserByUsername(String username) {
@@ -31,9 +36,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
 
     @Override
     public Boolean hasUsername(String username) {
-        UserDO userDO =  baseMapper.selectOne(Wrappers.lambdaQuery(UserDO.class).eq(UserDO::getUsername, username));
-
-        return userDO != null;
+        return userRegisterCachePenetrationBloomFilter.contains(username);
     }
 }
 
