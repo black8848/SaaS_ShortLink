7e4784a2089c5b47b1e472ba26dcfc0d8b5a7dec
black8848 <fuzixian666@gmail.com>
2025-01-03 17:35:42 +0800

    添加 MyBatis-Plus 分页插件

---
+++ project/src/main/java/org/example/project/config/DataBaseConfiguration.java
@@ -0,0 +1,22 @@
+package org.example.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 添加分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
