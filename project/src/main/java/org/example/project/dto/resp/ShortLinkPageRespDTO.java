0d7235f0e65cbc9b81516bf474bc56c8b57a229b
black8848 <fuzixian666@gmail.com>
2025-01-03 19:55:17 +0800

    短链接分页接口新增创建时间以及时间序列化

--- project/src/main/java/org/example/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/org/example/project/dto/resp/ShortLinkPageRespDTO.java
@@ -56,7 +56,7 @@ public class ShortLinkPageRespDTO {
      * 创建时间
      */
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
-    private Date create_time;
+    private Date createTime;
 
     /**
      * 描述
